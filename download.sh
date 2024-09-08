24bacbfc823aea629958f6f824c3193917a30a23
Nicol <nicolechantzi@gmail.com>
2024-09-08 22:09:30 +0300

    pipeline for GFF

---
+++ download.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wget ftp.ensembl.org/pub/current_gff3/sus_scrofa/Sus_scrofa.Sscrofa11.1.112.gff3.gz
---
+++ pipeline.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+file=$(basename $1 .gz)
+
+if [[ ! -f $file ]]; then
+    if ! wget "$1"; then
+        echo "Download failed."
+        exit 1
+    fi
+    gunzip $file.gz
+    echo "File has been downloaded!"
+else
+    echo "File already exists!"
+fi
+
+features=$(cat $file | grep -v '#' | wc -l)
+echo "Total features: ${features}."
+
+regions=$(awk -F '\t' '/^##sequence-region/ { count++ } END { print count }' $file)
+echo "Total sequence regions: ${regions}."
+
+genes=$(awk -F '\t' '{ if ($3 ~ "gene") count++ } END { print count }' $file)
+echo "Total genes: ${genes}."
+
+topTen=$(awk '!/^#/ { counts[$3]++ } END { for (item in counts) print counts[item] "\t" item }' $file | sort -nr | head -10)
+echo -e "Top-ten most annotated feature types:\n${topTen}"
